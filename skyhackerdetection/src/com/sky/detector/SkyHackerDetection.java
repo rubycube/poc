6afc1e954c456d34f030d8170e78443c6f89da8d
rubycube <email4sabu@gmail.com>
2015-02-18 21:42:25 +0000

    clean the main

--- skyhackerdetection/src/com/sky/detector/SkyHackerDetection.java
+++ skyhackerdetection/src/com/sky/detector/SkyHackerDetection.java
@@ -15,22 +15,10 @@ public class SkyHackerDetection {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        HackerDetectorImpl hackerDetector = new HackerDetectorImpl();
-        String[] logLineArray = {
-            "80.238.9.179,123612947,SIGNIN_FAILURE,Dave.Branning",
-            "80.238.9.179,133612947,SIGNIN_FAILURE,Dave.Branning",
-            "80.238.9.179,133612947,SIGNIN_FAILURE,Dave.Branning",
-            "80.238.9.179,133612947,SIGNIN_SUCCESS,Dave.Branning",
-            "80.238.9.179,133612947,SIGNIN_FAILURE,Dave.Branning",
-            "80.238.9.179,133612947,SIGNIN_FAILURE,Dave.Branning",
-            "80.238.9.179,133612947,SIGNIN_FAILURE,Dave.Branning"};
-        String ip;
-        for (String logLine : logLineArray) {
-            if (null != logLine) {
-                ip = hackerDetector.parseLine(logLine);
-                if (null != ip) {
-                    System.out.println("Hacker detected: " + ip);
-                }
+        if (args.length > 0) {
+            String ip = new HackerDetectorImpl().parseLine(args[0].trim());
+            if (null != ip) {
+                System.out.println("Hacker detected: " + ip);
             }
         }
     }
